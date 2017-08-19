2d7cb32373e493c4669947bc590a1533de302025
Sidharth Ramesh <tornadoalert@gmail.com>
2017-08-19 15:55:08 +0530

    disabled static file compression

--- kmcoffice/settings.py
+++ kmcoffice/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/list'
 
