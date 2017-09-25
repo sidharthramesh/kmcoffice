268b34345582c153042b175bba438e43cc54c5ad
Sidharth Ramesh <tornadoalert@gmail.com>
2017-09-25 19:08:02 +0530

    added event calander to admin

--- venue/admin.py
+++ venue/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Booking,Venue
+from .models import Booking,Venue,EventCalander
 # Register your models here.
 admin.site.register(Venue)
+admin.site.register(EventCalander)
