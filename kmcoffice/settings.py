cde64a832743e2cd30cad8fa5eb4221b3be81031
Sidharth Ramesh <tornadoalert@gmail.com>
2017-10-17 12:26:49 +0530

    production mode

--- kmcoffice/settings.py
+++ kmcoffice/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j4s@@mg#ax$e8@el)(&zm4d0bcv-ih!)!!4yhrw22@nj1f!!-)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
